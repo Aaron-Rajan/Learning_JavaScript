0ff3d7953f951ee2fbf5ba0e99c73e746f97ad5c
Aaron <aaronmraj@gmail.com>
2022-01-04 17:52:21 -0500

    Completed chapters 1 and 2

---
+++ Chap_2/sandbox.js
@@ -0,0 +1,42 @@
+let first_name = "Aaron";
+let last_name = "Rajan";
+let middle_name = "Matthew";
+let full_name = first_name + " " + middle_name + " " + last_name;
+
+// let result = full_name.slice(5,10);
+// console.log(result);
+
+// console.log(full_name);
+// console.log("HI MY NAME IS " + full_name.toUpperCase() + "!!!!!");
+// console.log("Sorry for yelling, it's " + full_name.toLowerCase());
+
+// let money = 500;
+// let height = "5'8";
+
+// let result = `Hey, my name is ${full_name}. I have ${money} dollars, and I am ${height}.`;
+// console.log(result);
+
+// let names = ["Aaron", "Jacob", "Aidan", "Alan"];
+
+// console.log(names);
+// console.log(names.pop());
+// console.log(names.length);
+
+// let result = names.push("Jeffin");
+// let index = names.indexOf('Jeffin');
+// console.log(`Jeffin was added at index ${index}, changing array size to ${result}`);
+
+console.log(true, "true");
+
+let age = 19;
+
+console.log(age == 19);
+console.log(age == "19");
+console.log(age === "19");
+console.log(age !== "19");
+
+console.log(first_name > "Blake");
+console.log(last_name > "Blake");
+
+let age_2 = "19";
+console.log(Number(age_2));
